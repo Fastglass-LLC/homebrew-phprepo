468e06ab5018baef79665d1006eaec6f285c6edf
Brady Owens <info@fastglass.net>
2018-07-19 22:44:58 -0400

    Remove the PHP versions I am not going to support.

--- Abstract/abstract-php-version.rb
+++ Abstract/abstract-php-version.rb
@@ -6,61 +6,6 @@ class AbstractPhpVersion < Formula
     }.freeze
   end
 
-  module Php53Defs
-    PHP_SRC_TARBALL = "https://php.net/get/php-5.3.29.tar.bz2/from/this/mirror".freeze
-    PHP_GITHUB_URL  = "https://github.com/php/php-src.git".freeze
-    PHP_VERSION     = "5.3.29".freeze
-    PHP_BRANCH      = "PHP-5.3".freeze
-
-    PHP_CHECKSUM    = {
-      :sha256 => "c4e1cf6972b2a9c7f2777a18497d83bf713cdbecabb65d3ff62ba441aebb0091",
-    }.freeze
-  end
-
-  module Php54Defs
-    PHP_SRC_TARBALL = "https://php.net/get/php-5.4.45.tar.bz2/from/this/mirror".freeze
-    PHP_GITHUB_URL  = "https://github.com/php/php-src.git".freeze
-    PHP_VERSION     = "5.4.45".freeze
-    PHP_BRANCH      = "PHP-5.4".freeze
-
-    PHP_CHECKSUM    = {
-      :sha256 => "4e0d28b1554c95cfaea6fa2b64aac85433f158ce72bb571bcd5574f98f4c6582",
-    }.freeze
-  end
-
-  module Php55Defs
-    PHP_SRC_TARBALL = "https://php.net/get/php-5.5.38.tar.bz2/from/this/mirror".freeze
-    PHP_GITHUB_URL  = "https://github.com/php/php-src.git".freeze
-    PHP_VERSION     = "5.5.38".freeze
-    PHP_BRANCH      = "PHP-5.5".freeze
-
-    PHP_CHECKSUM    = {
-      :sha256 => "473c81ebb2e48ca468caee031762266651843d7227c18a824add9b07b9393e38",
-    }.freeze
-  end
-
-  module Php56Defs
-    PHP_SRC_TARBALL = "https://php.net/get/php-5.6.33.tar.bz2/from/this/mirror".freeze
-    PHP_GITHUB_URL  = "https://github.com/php/php-src.git".freeze
-    PHP_VERSION     = "5.6.33".freeze
-    PHP_BRANCH      = "PHP-5.6".freeze
-
-    PHP_CHECKSUM    = {
-      :sha256 => "07f696a9761dcd839e2045c95c3a4d2ffb52c54417477cca9d30a14975b831cc",
-    }.freeze
-  end
-
-  module Php70Defs
-    PHP_SRC_TARBALL = "https://php.net/get/php-7.0.27.tar.bz2/from/this/mirror".freeze
-    PHP_GITHUB_URL  = "https://github.com/php/php-src.git".freeze
-    PHP_VERSION     = "7.0.27".freeze
-    PHP_BRANCH      = "PHP-7.0".freeze
-
-    PHP_CHECKSUM    = {
-      :sha256 => "99fa2563bb4c4c1cde9febe87cfe97324227d7b4b8828f2e936e507127394131",
-    }.freeze
-  end
-
   module Php71Defs
     PHP_SRC_TARBALL = "https://php.net/get/php-7.1.14.tar.bz2/from/this/mirror".freeze
     PHP_GITHUB_URL  = "https://github.com/php/php-src.git".freeze
