363d8424ec9fd2bb87e9ea971b9b9187a5a35115
Brady Owens <info@fastglass.net>
2018-07-21 09:47:13 -0400

    Remove all bottle statements. The bottles were from when this was in Homebrew core.

--- Formula/php72.rb
+++ Formula/php72.rb
@@ -5,12 +5,6 @@ class Php72 < AbstractPhp
   desc "PHP Version 7.2"
   revision 13
 
-  bottle do
-    sha256 "fa8adca0854f31aac6d74d70539fabdf2394f3222138eed6a105bafd7a6d3abf" => :high_sierra
-    sha256 "2a4696ee084155b981cc7435e2ec3603c8d2e5917365ea85549078794f45070e" => :sierra
-    sha256 "3940c952e237dd1a5bd25d58888cff1a5af464a7c6d352a355bf95ee4273e81a" => :el_capitan
-  end
-
   include AbstractPhpVersion::Php72Defs
 
   url PHP_SRC_TARBALL
