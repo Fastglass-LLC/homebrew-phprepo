77f1685fa9609eb843034915b50352a6d964a113
Brady Owens <info@fastglass.net>
2018-07-22 22:21:08 -0400

    PHP 7.2 SOLR package.

---
+++ Formula/php72-solr.rb
@@ -0,0 +1,22 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php72Solr < AbstractPhp72Extension
+  init
+  desc "Fast and lightweight library to communicate with Apache Solr servers"
+  homepage "https://pecl.php.net/package/solr"
+  url "https://pecl.php.net/get/solr-2.4.0.tgz"
+  sha256 "22865dafb76fc5839e84a5bd423bb37d5062883e5dfc4d064b43129ac9f2752c"
+  head "https://git.php.net/repository/pecl/search_engine/solr.git"
+
+  def install
+    Dir.chdir "solr-#{version}" unless build.head?
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig
+    system "make"
+    prefix.install "modules/solr.so"
+    write_config_file if build.with? "config-file"
+  end
+end
