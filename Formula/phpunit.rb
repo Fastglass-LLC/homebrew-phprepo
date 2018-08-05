5fb6f6bb67f8e415fd47cf943281e5ba9eff8cd2
Brady Owens <info@fastglass.net>
2018-08-05 14:17:02 -0400

    Update PHPunit to version 7.3.0

--- Formula/phpunit.rb
+++ Formula/phpunit.rb
@@ -4,8 +4,8 @@ class Phpunit < AbstractPhpPhar
   init
   desc "Programmer-oriented testing framework for PHP"
   homepage "https://phpunit.de"
-  url "https://phar.phpunit.de/phpunit-7.0.3.phar"
-  sha256 "5889db48d1cd666ecd6b8885f47c0bdef1778e81658b5e636e3ada1d2bd382c0"
+  url "https://phar.phpunit.de/phpunit-7.3.0.phar"
+  sha256 "a544ef63a728e1e9a1c6eaa91bff203a2f265d0462540b1c52ee1e46436e303f"
 
   bottle :unneeded
 
