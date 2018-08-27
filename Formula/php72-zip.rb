3f27b352887d05af376a4a33b0625cf08919ddb7
Brady Owens <info@fastglass.net>
2018-08-27 08:52:15 -0400

    Error in Zip extension formula.

--- Formula/php72-zip.rb
+++ Formula/php72-zip.rb
@@ -6,14 +6,13 @@ class Php72Zip < AbstractPhp72Extension
   homepage "https://pecl.php.net/package/zip"
   url "https://pecl.php.net/get/zip-1.15.3.tgz"
   sha256 "6766799a8fb09f2990224fc7d40e01737294a515d3ffd8895fa25249c31ba0a7"
-  head "https://github.com/php/pecl-file_formats-yaml.git", :branch => "php7"
   revision 1
 
 
   depends_on "libzip"
 
   def install
-    Dir.chdir "yaml-#{version}" unless build.head?
+    Dir.chdir "zip-#{version}" unless build.head?
 
     safe_phpize
     system "./configure", phpconfig
