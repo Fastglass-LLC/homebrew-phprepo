955c67aaca8cbe0d98def4de3832cdab26f97b14
Brady Owens <info@fastglass.net>
2018-10-20 22:42:17 -0400

    Update Symfondy installer

--- Formula/symfony-installer.rb
+++ Formula/symfony-installer.rb
@@ -7,16 +7,9 @@ class SymfonyInstaller < Formula
   desc "Create Symfony framework projects"
   homepage "https://github.com/symfony/symfony-installer"
   url "https://github.com/symfony/symfony-installer.git",
-    :tag => "v1.5.10",
-    :revision => "e3bf2ed78bbea1dce9f5454a136e5895b313d03a"
+    :tag => "v1.5.11",
+    :revision => "bd73c8b50f471f4e219dacfc40e03a0aca442717"
   head "https://github.com/symfony/symfony-installer.git"
-
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "df646f411d5bf06312ec9aa6ddf150563fdc05040691fa834a44bb6bba8230f7" => :high_sierra
-    sha256 "ec1e33d375425a74e0df848522c67b3ae37d62a14afa4a094cbccec12d71cc63" => :sierra
-    sha256 "6d3d018c9f5d121b618a3a2c130587503ce4c09f4a785a455ae3c6bfbd267d33" => :el_capitan
-  end
 
   depends_on PhpMetaRequirement
   depends_on "box" => :build
