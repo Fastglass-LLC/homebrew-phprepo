b9c1c2bd9ee4342c5ad6fc609f53142d6c10b781
Brady Owens <info@fastglass.net>
2018-11-05 08:30:21 -0500

    Update Swoole to latest release

--- Formula/php72-swoole.rb
+++ Formula/php72-swoole.rb
@@ -4,10 +4,9 @@ class Php72Swoole < AbstractPhp72Extension
   init
   desc "Event-driven asynchronous & concurrent networking engine for PHP."
   homepage "https://pecl.php.net/package/swoole"
-  url "https://pecl.php.net/get/swoole-4.2.5.tgz"
-  sha256 "d1ec03bd493c9058e547b401acdd1ea6ad1a8b4ee57884add711974bc01567ac"
+  url "https://pecl.php.net/get/swoole-4.2.6.tgz"
+  sha256 "4481dbede665f8e3a8010943eea81b788d1567b2a42dd3aecbfbb1ecdf5508df"
   head "https://github.com/swoole/swoole-src.git"
-  revision 1
 
 
   def install
