0966f775cb98e4dbf222b417bdf26595ba40a355
Brady Owens <info@fastglass.net>
2018-11-11 07:48:26 -0500

    Update Mustache for PHP 7.2

--- Formula/php72-mustache.rb
+++ Formula/php72-mustache.rb
@@ -4,8 +4,8 @@ class Php72Mustache < AbstractPhp72Extension
   init
   desc "Mustache PHP Extension"
   homepage "https://github.com/jbboehr/php-mustache#mustache"
-  url "https://github.com/jbboehr/php-mustache/archive/v0.7.3.tar.gz"
-  sha256 "69a6cbe5ae472c0fb1db8979fe0784caeb9d9baace3e13aaa7ab68f0a34dd83e"
+  url "https://pecl.php.net/get/mustache-0.8.0.tgz"
+  sha256 "9f83f3be1f6cb2bb52cf74b40458605ae02611119e312e5a3e31ffe75c3f90e7"
   head "https://github.com/jbboehr/php-mustache.git"
 
 
