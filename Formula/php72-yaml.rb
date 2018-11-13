f11fa3142aa46ceff526967e5b119683d8209244
Brady Owens <info@fastglass.net>
2018-11-13 07:52:51 -0500

    Yaml 2.0.3 for PHP 7.2

--- Formula/php72-yaml.rb
+++ Formula/php72-yaml.rb
@@ -4,10 +4,9 @@ class Php72Yaml < AbstractPhp72Extension
   init
   desc "YAML-1.1 parser and emitter"
   homepage "https://pecl.php.net/package/yaml"
-  url "https://pecl.php.net/get/yaml-2.0.0.tgz"
-  sha256 "ef13ff56c184290c025a522bf9ae2e1b3ecc8543c3a5161dd02adec90897a221"
+  url "https://pecl.php.net/get/yaml-2.0.3.tgz"
+  sha256 "a94e3f49b7df748f903fdd22e4acd091b9234f5ba47ca4043c442d7667c7dc3b"
   head "https://github.com/php/pecl-file_formats-yaml.git", :branch => "php7"
-  revision 1
 
 
   depends_on "libyaml"
