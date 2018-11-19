516e023a1b75aff0f0f33d02df9eb615bc0a0040
Brady Owens <info@fastglass.net>
2018-11-18 21:33:04 -0500

    Upgrade DS for PHP 7.2

--- Formula/php72-ds.rb
+++ Formula/php72-ds.rb
@@ -4,8 +4,8 @@ class Php72Ds < AbstractPhp72Extension
   init
   desc "Data Structures for PHP"
   homepage "https://github.com/php-ds/extension"
-  url "https://github.com/php-ds/extension/archive/v1.2.4.tar.gz"
-  sha256 "c080bb08445fe690da2271ff866602cf27cadee529ab1a1edbf4aa7a1d6e104c"
+  url "https://pecl.php.net/get/ds-1.2.7.tgz"
+  sha256 "607a52d694eb6484086b0782e759df340363338ac2646966aac32d263a4ee81c"
   head "https://github.com/php-ds/extension.git"
 
   def install
