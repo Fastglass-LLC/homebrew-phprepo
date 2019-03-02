87e31ead2c2d14717d1aa276bc6937f2254a8fe1
Brady Owens <info@fastglass.net>
2019-03-02 07:49:21 -0500

    PHP72 - gRPC update

--- Formula/php72-grpc.rb
+++ Formula/php72-grpc.rb
@@ -4,8 +4,8 @@ class Php72Grpc < AbstractPhp72Extension
   init
   desc "The PHP extension for the gRPC library"
   homepage "https://grpc.io"
-  url "https://pecl.php.net/get/grpc-1.18.0.tgz"
-  sha256 "c176b30987f4c8d76e92b6fb401bbb2dadceca20feeb2fd0714675d0cec26ece"
+  url "https://pecl.php.net/get/grpc-1.19.0.tgz"
+  sha256 "172b8f3d30aa286ce6f3bb89a25ee1aa0976de139233bd14fb36564dd6ef8435"
 
   def install
     Dir.chdir "grpc-#{version}"
