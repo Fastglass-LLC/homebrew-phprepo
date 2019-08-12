c2307758c187ae3cd61531e85c06c29e1dd73cac
Brady Owens <info@fastglass.net>
2019-08-12 18:43:25 -0400

    PHP72 PHP73 Timezone update

--- Formula/php73-timezonedb.rb
+++ Formula/php73-timezonedb.rb
@@ -4,8 +4,8 @@ class Php73Timezonedb < AbstractPhp73Extension
   init
   desc "Timezone Database to be used with PHP's date & time functions"
   homepage "https://pecl.php.net/package/timezonedb"
-  url "https://pecl.php.net/get/timezonedb-2018.9.tgz"
-  sha256 "661364836f91ec8b5904da4c928b5b2df8cb3af853994f8f4d68b57bc3c32ec8"
+  url "https://pecl.php.net/get/timezonedb-2019.2.tgz"
+  sha256 "80921bf6a8235925320773a24f23f2b5cdef5948d0e4f468c67384b351f243cc"
   head "https://svn.php.net/repository/pecl/timezonedb/trunk/"
   revision 1
 
