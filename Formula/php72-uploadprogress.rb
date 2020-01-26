d45f8f820cc68494a2c0014ccf3ada82aac0a3c2
Brady Owens <info@fastglass.net>
2020-01-26 11:39:49 -0500

    PHP72 PHP73 Uploadprogress is now compat with PHP7

---
+++ Formula/php72-uploadprogress.rb
@@ -0,0 +1,20 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php72Uploadprogress < AbstractPhp73Extension
+  init
+  desc "An extension to track progress of a file upload"
+  homepage "https://github.com/php/pecl-php-uploadprogress"
+  url "https://pecl.php.net/get/uploadprogress-1.1.0.tgz"
+  sha256 "086ff9e52e8851f068254d5bf62e0128fc047c25ff2f6eff00ac89425ab81412"
+  revision 1
+
+  def install
+    Dir.chdir "uploadprogress-#{version}" unless build.head?
+
+    safe_phpize
+    system "./configure", phpconfig
+    system "make"
+    prefix.install "modules/uploadprogress.so"
+    write_config_file if build.with? "config-file"
+  end
+end
