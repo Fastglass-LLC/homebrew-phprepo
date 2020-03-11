5d4253c12545d4f658c77ddbe1c6bf70f91bbc17
Brady Owens <info@fastglass.net>
2020-03-11 12:23:38 -0400

    PHP72 PHP73 Mongodb update

--- Formula/php73-mongodb.rb
+++ Formula/php73-mongodb.rb
@@ -4,8 +4,8 @@ class Php73Mongodb < AbstractPhp73Extension
   init
   desc "MongoDB driver for PHP."
   homepage "https://pecl.php.net/package/mongodb"
-  url "https://pecl.php.net/get/mongodb-1.7.3.tgz"
-  sha256 "bbe8992f9b26815e51e7a0fccfe39340ee0702a0adbc06dc9c420e746ae61d2c"
+  url "https://pecl.php.net/get/mongodb-1.7.4.tgz"
+  sha256 "1cec8be860d7d6cff05b075a0e0fc47b1d9f580ef6a0cd46948e455a88a6859a"
   head "https://github.com/mongodb/mongo-php-driver.git"
 
 
