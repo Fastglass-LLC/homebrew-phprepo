2efe242b687cee16a233627c808fcebef0aa47ee
Brady Owens <brady.owens@lfg.com>
2020-04-15 09:34:14 -0400

    PHP74 Zip initial commit

---
+++ Formula/php74-zip.rb
@@ -0,0 +1,23 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php74Zip < AbstractPhp74Extension
+  init
+  desc "A zip management extension"
+  homepage "https://pecl.php.net/package/zip"
+  url "https://pecl.php.net/get/zip-1.18.1.tgz"
+  sha256 "08240815eab68d932f74586bdb7f095faf4cc28a3698ea6f1f78498140f32fc0"
+  revision 1
+
+
+  depends_on "libzip"
+
+  def install
+    Dir.chdir "zip-#{version}" unless build.head?
+
+    safe_phpize
+    system "./configure", phpconfig
+    system "make"
+    prefix.install "modules/zip.so"
+    write_config_file if build.with? "config-file"
+  end
+end
