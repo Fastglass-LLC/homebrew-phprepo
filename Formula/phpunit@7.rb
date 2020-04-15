18d78c9a45f7ed06e96b2f4c8cc853036f05c22c
Brady Owens <brady.owens@lfg.com>
2020-04-15 10:07:50 -0400

    PHPunit upgrade and create back-version formulas

--- Formula/phpunit@7.rb
+++ Formula/phpunit@7.rb
@@ -1,11 +1,11 @@
 require File.expand_path("../../Abstract/abstract-php-phar", __FILE__)
 
-class Phpunit < AbstractPhpPhar
+class PhpunitAT7 < AbstractPhpPhar
   init
   desc "Programmer-oriented testing framework for PHP"
   homepage "https://phpunit.de"
-  url "https://phar.phpunit.de/phpunit-9.1.1.phar"
-  sha256 "f753fa2e6ec4825f9b729073020f21f0a5150a2345012341154da4555c1b83bb"
+  url "https://phar.phpunit.de/phpunit-7.5.9.phar"
+  sha256 "5404288061420c3921e53dd3a756bf044be546c825c5e3556dea4c51aa330f69"
 
   bottle :unneeded
 
--- Formula/phpunit@8.rb
+++ Formula/phpunit@8.rb
@@ -1,11 +1,11 @@
 require File.expand_path("../../Abstract/abstract-php-phar", __FILE__)
 
-class PhpunitAT65 < AbstractPhpPhar
+class PhpunitAT8 < AbstractPhpPhar
   init
-  desc "(Old Stable) Programmer-oriented testing framework for PHP"
+  desc "Programmer-oriented testing framework for PHP"
   homepage "https://phpunit.de"
-  url "https://phar.phpunit.de/phpunit-6.5.6.phar"
-  sha256 "ad0d5f8dfbd95ffcbfbe7fde4fa7a9d4f24fa6e070b1215faea579cdefac08f3"
+  url "https://phar.phpunit.de/phpunit-8.5.3.phar"
+  sha256 "5556b6dc5971bc2d38c47a04877221562a09cc10d7dbf4fbfff9909b28879616"
 
   bottle :unneeded
 
