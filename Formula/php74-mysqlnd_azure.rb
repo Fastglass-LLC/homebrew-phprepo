9470a91b6fdc704ab79d7308d3c88c313978ed0c
Brady Owens <brady.owens@lfg.com>
2020-04-15 10:02:09 -0400

    PHP74 MySQLnd Azure connector initial commit

---
+++ Formula/php74-mysqlnd_azure.rb
@@ -0,0 +1,20 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php74MysqlndAzure < AbstractPhp74Extension
+  init
+  desc "MySQLnd redirection extension for Azure"
+  homepage "https://github.com/microsoft/mysqlnd_azure"
+  url "https://pecl.php.net/get/mysqlnd_azure-1.0.0.tgz"
+  sha256 "490fb739fa41c019d542226e8e7a8178b0bea3c5e4ef2c6d22a4ca6505035a9f"
+  head "https://github.com/microsoft/mysqlnd_azure.git"
+
+
+
+  def install
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig
+    system "make"
+    prefix.install "modules/mysqlnd_azure.so"
+    write_config_file if build.with? "config-file"
+  end
+end
