c6adee3a5f2700487c4698761cfa6ec17314b978
Brady Owens <brady.owens@lfg.com>
2020-04-16 06:34:52 -0400

    Drupal Console update

--- Formula/drupalconsole.rb
+++ Formula/drupalconsole.rb
@@ -4,8 +4,8 @@ class Drupalconsole < AbstractPhpPhar
   init
   desc "CLI tool for Drupal 8"
   homepage "https://drupalconsole.com/"
-  url "https://github.com/hechoendrupal/drupal-console-launcher/releases/download/1.8.0/drupal.phar"
-  sha256 "1c9bef3d3e4f70633d5004e3a3d86d90630df7cf97dab46d0175855796f934da"
+  url "https://github.com/hechoendrupal/drupal-console-launcher/releases/download/1.9.4/drupal.phar"
+  sha256 "968b03813b036067114e479b94b711b488bc40ef84e56cc2775f28cd247bc65b"
 
   def install
     bin.install "drupal.phar" => "drupal"
