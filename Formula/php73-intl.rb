e535535288b730774ad6244e757eca113d1595a9
Brady Owens <info@fastglass.net>
2020-10-09 19:52:32 -0400

    PHP73 - Intl module will modify the php.ini. Adding the conf file creates a redundency

--- Formula/php73-intl.rb
+++ Formula/php73-intl.rb
@@ -4,7 +4,7 @@ class Php73Intl < AbstractPhp73Extension
   init
   desc "Wrapper for the ICU library"
   homepage "https://php.net/manual/en/book.intl.php"
-  revision 26
+  revision 27
 
   url PHP_SRC_TARBALL
   sha256 PHP_CHECKSUM[:sha256]
@@ -15,28 +15,16 @@ class Php73Intl < AbstractPhp73Extension
 
     # icu4c 61.1 compatability
     ENV.append "CPPFLAGS", "-DU_USING_ICU_NAMESPACE=1"
-    
+
     Dir.chdir "ext/intl"
 
     safe_phpize
     system "./configure", "--prefix=#{prefix}",
-                          phpconfig,
-                          "--disable-dependency-tracking",
-                          "--enable-intl",
-                          "--with-icu-dir=#{Formula["icu4c"].prefix}"
+           phpconfig,
+           "--disable-dependency-tracking",
+           "--enable-intl",
+           "--with-icu-dir=#{Formula["icu4c"].prefix}"
     system "make"
     prefix.install "modules/intl.so"
-    write_config_file if build.with? "config-file"
-  end
-
-  def config_file
-    super + <<~EOS
-
-      ;intl.default_locale =
-      ; This directive allows you to produce PHP errors when some error
-      ; happens within intl functions. The value is the level of the error produced.
-      ; Default is 0, which does not produce any errors.
-      ;intl.error_level = E_WARNING
-    EOS
   end
 end
