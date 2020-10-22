7f7ef40cb89822ae8079a6ddee69c6c605883a43
Brady Owens <info@fastglass.net>
2020-10-22 08:05:10 -0400

    PHP72 PHP73 PHP74 igbinary updates to formula and release

--- Formula/php73-igbinary.rb
+++ Formula/php73-igbinary.rb
@@ -4,12 +4,11 @@ class Php73Igbinary < AbstractPhp73Extension
   init
   desc "Igbinary is a replacement for the standard php serializer."
   homepage "https://pecl.php.net/package/igbinary"
-  url "https://pecl.php.net/get/igbinary-3.0.1.tgz"
-  sha256 "5959607f3e236e19a9d01e1a8c74800fb3138f3528ba6601deedbd8b83ab12f1"
+  url "https://pecl.php.net/get/igbinary-3.1.6.tgz"
+  sha256 "b9bf87dc4d861c6769ae2a13b085395e2741505c629895ec4559781a5031fdea"
   head "https://github.com/igbinary/igbinary.git"
 
 
-  depends_on "igbinary" => :build
 
   def install
     Dir.chdir "igbinary-#{version}" unless build.head?
