68ef37e113dce7af22e17debcd637d8034b25884
Brady Owens <info@fastglass.net>
2020-10-24 10:04:37 -0400

    Class named wrong

--- Formula/composer@1.rb
+++ Formula/composer@1.rb
@@ -1,5 +1,5 @@
 
-class Composer < Formula
+class ComposerAT08 < Formula
   desc "Dependency Manager for PHP"
   homepage "https://getcomposer.org/"
   url "https://getcomposer.org/download/1.10.15/composer.phar"
