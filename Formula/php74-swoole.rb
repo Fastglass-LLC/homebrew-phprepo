a8fd49f1275132d7d114b5957c1f693a31294166
Brady Owens <info@fastglass.net>
2020-11-27 12:08:41 -0500

    PHP72 PHP73 PHP74 Swoole update

--- Formula/php74-swoole.rb
+++ Formula/php74-swoole.rb
@@ -4,8 +4,8 @@ class Php74Swoole < AbstractPhp74Extension
   init
   desc "Event-driven asynchronous & concurrent networking engine for PHP."
   homepage "https://pecl.php.net/package/swoole"
-  url "https://pecl.php.net/get/swoole-4.5.8.tgz"
-  sha256 "202efb1ca36b7d30a54ee7571c7db34a5bf73a983a2c97f68cf074891a8f12c9"
+  url "https://pecl.php.net/get/swoole-4.5.9.tgz"
+  sha256 "8991012db81138a1cd825860ba1628e8f1b799573ad16aeed82a955b6fe72e3d"
   head "https://github.com/swoole/swoole-src.git"
 
 
