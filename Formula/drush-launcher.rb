79d8f4dbdbeb7a7f885f0903a966902b9b406ad0
Brady Owens <brady@fastglass.net>
2021-11-20 20:38:02 -0500

    Update Drush Launcher

--- Formula/drush-launcher.rb
+++ Formula/drush-launcher.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Abstract/abstract-php-phar", __FILE__)
 class DrushLauncher < AbstractPhpPhar
   desc "A small wrapper around Drush for your global $PATH."
   homepage "https://github.com/drush-ops/drush-launcher"
-  url "https://github.com/drush-ops/drush-launcher/releases/download/0.6.0/drush.phar"
-  sha256 "c3f32a800a2f18470b0010cd71c49e49ef5c087f8131eecfe9b686dc1f3f3d4e"
+  url "https://github.com/drush-ops/drush-launcher/releases/download/0.9.1/drush.phar"
+  sha256 "52b5e22031789e087e4f79ac49c74d94c0379ad6859f3ec14671ce1647a3b7a6"
 
   def phar_file
     "drush.phar"
