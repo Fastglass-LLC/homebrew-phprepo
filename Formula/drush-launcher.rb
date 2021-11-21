6170e2f74347ca84c1ff79fa53deb89922670c6d
Brady Owens <brady@fastglass.net>
2021-11-20 20:35:15 -0500

    Deprecatted call

--- Formula/drush-launcher.rb
+++ Formula/drush-launcher.rb
@@ -6,8 +6,6 @@ class DrushLauncher < AbstractPhpPhar
   url "https://github.com/drush-ops/drush-launcher/releases/download/0.6.0/drush.phar"
   sha256 "c3f32a800a2f18470b0010cd71c49e49ef5c087f8131eecfe9b686dc1f3f3d4e"
 
-  bottle :unneeded
-
   def phar_file
     "drush.phar"
   end
